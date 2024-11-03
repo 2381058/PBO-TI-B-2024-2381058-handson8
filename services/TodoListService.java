e3272f483b472ba38016c4a32bbaaa023679ce57
User <2381058@unai.edu>
2024-11-03 10:03:18 +0700

    Clean Architecture Implementation

---
+++ services/TodoListService.java
@@ -0,0 +1,10 @@
+package services;
+
+import entities.TodoList;
+
+public interface TodoListService {
+    TodoList[] getTodoList();
+    void addTodoList(String Todo);
+    Boolean removeTodoList(Integer number);
+    Boolean editTodoList(Integer number, String Todo);
+}
